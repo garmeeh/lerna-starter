07a32ff7c47c39d8f4bb334203ff69c5873a8e59
Benjamin Gudehus <hastebrot@gmail.com>
2019-06-28 22:05:12 +0200

    (feature) Add markdown story.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import React from "react"
-import { configure } from "@storybook/react"
+import { load, addParameters } from "@storybook/react"
+import { themes } from "@storybook/theming"
+import { version } from "../package.json"
 
-const req = require.context("../packages", true, /.story.(jsx?|js?)$/)
-function loadStories() {
-  req.keys().forEach(filename => req(filename))
-}
+addParameters({
+  options: {
+    name: "storybook",
+    theme: themes.light,
+    showPanel: false,
+    sidebarAnimations: false,
+    isToolshown: true,
+    storySort: (story, otherStory) => story[1].id.localeCompare(otherStory[1].id),
+  },
+})
 
-configure(loadStories, module)
+load(require.context("../packages", true, /\.story\.js$/), module)
+load(require.context("../packages", true, /\.story\.mdx$/), module)
